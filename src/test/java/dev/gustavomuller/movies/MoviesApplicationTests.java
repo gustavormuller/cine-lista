33639ee31aa0d1fd10e6c9e47b245dd19df57793
unknown <gusrmuller@gmail.com>
2023-11-19 04:19:31 -0300

    setup spring initializr

---
+++ src/test/java/dev/gustavomuller/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavomuller.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
