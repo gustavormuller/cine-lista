a71bfb49d70eed1926208411c7bdc7f435b3c04c
unknown <gusrmuller@gmail.com>
2023-11-28 05:05:16 -0300

    fix CORS error

--- src/main/java/dev/gustavomuller/movies/MoviesApplication.java
+++ src/main/java/dev/gustavomuller/movies/MoviesApplication.java
@@ -2,6 +2,9 @@ package dev.gustavomuller.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,5 +14,13 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
